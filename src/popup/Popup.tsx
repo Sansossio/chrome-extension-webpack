f86fa51d217dddb007976f9af52ed4d795933f60
Sansossio <juliosansossio@gmail.com>
2020-06-26 20:42:22 +0200

    print current url

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,11 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Popup.scss'
 
-export default function Popup () {
-  useEffect(() => {
-    // Example of how to send a message to eventPage.ts.
-    chrome.runtime.sendMessage({ popupMounted: true })
-  }, [])
+export default class Popup extends React.Component<{}, any> {
+  constructor (props) {
+    super(props)
+    this.state = {
+      currentUrl: null
+    }
+  }
 
-  return <div className='popupContainer'>Testing</div>
+  componentDidMount () {
+    const params: chrome.tabs.QueryInfo = {
+      active: true,
+      currentWindow: true
+    }
+    chrome.tabs.query(params, (tabs) => {
+      const { url } = tabs[0]
+      this.setState({ currentUrl: url })
+    })
+  }
+
+  render () {
+    return (
+      <div className='popupContainer'>
+        <h1>Testing</h1>
+        <p>Url: {this.state.currentUrl}</p>
+      </div>
+    )
+  }
 }
