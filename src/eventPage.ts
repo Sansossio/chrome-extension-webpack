1f75c9de0a91224e948cfa7ba3b573133d656b97
Sansossio <juliosansossio@gmail.com>
2020-06-26 20:42:17 +0200

    remove useless event

--- src/eventPage.ts
+++ src/eventPage.ts
@@ -3,9 +3,5 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     // onMessage must return "true" if response is async.
   let isResponseAsync = false
 
-  if (request.popupMounted) {
-    console.log('eventPage notified that Popup.tsx has mounted.')
-  }
-
   return isResponseAsync
 })
